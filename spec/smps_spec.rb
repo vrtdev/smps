ec2b878fb8acb4a6f4c5882f3f8494f2da5b0324
Jan Vansteenkiste <jan@vstone.eu>
2018-10-03 07:09:55 +0200

    Rubocop fixes: single quotes

--- spec/smps_spec.rb
+++ spec/smps_spec.rb
@@ -1,13 +1,14 @@
 # frozen_string_literal: true
+
 require 'spec_helper'
 require 'smps'
 
 RSpec.describe SmPs do
-  it "::VERSION" do
+  it '::VERSION' do
     expect(described_class::VERSION).not_to be nil
   end
 
-  context ".new" do
+  context '.new' do
     it do
       expect(SmPs.new).to be_a(SmPs::Client)
     end
